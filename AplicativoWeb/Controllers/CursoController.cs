59de630cd7180926dbc4a7ddb346830c7f706194
Abraão <abraao.l.silva6@aluno.senai.br>
2023-04-11 17:30:29 -0300

    revisao

---
+++ AplicativoWeb/Controllers/CursoController.cs
@@ -0,0 +1,20 @@
+using AplicativoWeb.Models;//camada model 
+using Microsoft.AspNetCore.Mvc; //microsoft
+namespace AplicativoWeb.Controllers; // Definindo
+
+// criamos a classe Controller 
+public class CursoController:Controller{
+public IActionResult Index() {
+    Curso c1 = new Curso ();
+    //set
+    c1.Descricao = "Primeiros Passos no ASP.NET";
+    c1.TituloInterno = "Desenvolvedor c#";
+    c1.TituloMarketing = "Crie Aplicativos Incríveis";
+
+    //get
+    ViewData ["desc"] = c1.Descricao;
+    ViewData["titInt"] = c1.TituloInterno;
+    ViewData["titMark"] = c1.TituloMarketing;
+return View();
+}
+}
---
+++ AplicativoWeb/Models/Curso.cs
@@ -0,0 +1,14 @@
+namespace AplicativoWeb.Models;
+
+public class Curso{
+
+
+    public int id { get; set; }
+
+    public string? TituloMarketing { get; set; }
+
+    public string? TituloInterno { get; set; }
+
+    public string? Descricao { get; set; }
+    
+}
